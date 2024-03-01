5bd92aeb0ec15108d74e1b08dd3b4f400efdf043
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2024-03-01 09:15:03 +0530

    a

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const jsonfile=require('jsonfile');
 const moment=require('moment');
-const simplegit=require('simple-git');
-
+const simpleGit=require('simple-git');
+// const random=require('random');
 
 const FILE_PATH='./data.json';
-const DATE=moment().subtract(3,'d').format();
-const  data={
-    date:DATE
-};
 
-jsonfile.writeFile(FILE_PATH,data)
-simplegit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+    const DATE=moment().subtract(1,'d').format();
+    const  data={
+        date:DATE+1
+    };
+    jsonfile.writeFile(FILE_PATH,data, () =>{
+        simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push();
+
+    });
+
+
+
+
+
+
